6eaecf3bbf7b934dee296e7cffc15b36c008ca40
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-25 10:45:47 -0300

    ok

--- mail/contact.php
+++ mail/contact.php
@@ -1,4 +1,40 @@
 <?php
+
+  require("src/PHPMailer.php");
+  require("src/SMTP.php");
+  require('src/Exception.php');
+
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
+
+    $mail = new PHPMailer(true);
+    $mail->IsSMTP(); // enable SMTP
+
+    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    // $email->SMTPAuth = true;
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "renatoguara2020@gmail.com";
+    $mail->Password = "ag";
+    $mail->SetFrom("renatoguara2020@gmail.com");
+    $mail->Subject = "PHP MAILER";
+    $mail->Body = "HELLO WORLD COM RENATO ALVES SOARES COM PHP MAILER E NATHANZINHO";
+    $mail->AddAddress("renatoguara2019@yahoo.com");
+
+     if(!$mail->Send()) {
+        echo "Mailer Error:  $mail->ErrorInfo ";
+     } else {
+        echo "<h1>Message has been sent to renatoguara2020</h1>";
+     }
+?>
+
+
+
+<?php
 if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
